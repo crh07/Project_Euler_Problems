84b62875242e9d7ab089fc0b8e1e09994086ba00
Christine <christine.herlihy@gmail.com>
2016-01-07 15:03:38 -0500

    Palindrome function now works

--- Euler_Problem4.py
+++ Euler_Problem4.py
@@ -2,32 +2,43 @@
 # The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-def palindrome(num, counter):
-    """
+# A palindromic number reads the same both ways.
+# The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
+# Find the largest palindrome made from the product of two 3-digit numbers.
 
-    :rtype : boolean
+def palindrome(num):
+    """
+   :rtype : boolean
     """
     nums = str(num)
 
-    # A one-digit number is a palindrome
+    # Recursive stopping rule: a one-digit number is a palindrome; even numbers will result in an empty string
 
     if len(nums) == 1 | 0:
+        print("true")
+        return True
+
+    elif len(nums) == 2:
+        if nums[0] == nums[1]:
             print("true")
             return True
 
     else:
+        if nums[0] == nums[len(nums) - 1]:
 
-            if nums[0] == nums[len(nums)-1]:
-                counter += 1
-                print(counter)
-                temp = nums[counter:-1]
-                print(temp)
-                palindrome(nums[counter:-1], counter)
-            else:
-                print("false")
-                return False
+            nums = nums[1:len(nums) - 1]
+
+            # Recursive function call
+            palindrome(nums)
+
+        else:
+            print("false")
+            return False
 
 
+# Test cases:
+mylist = [90109, 0, 401, 33, 696, 100, 2442, 35653, 190]
 
-mynum = 90109
-palindrome(mynum, 0)
+for x in mylist:
+    print(x)
+    palindrome(x)

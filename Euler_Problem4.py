66d795f90184156d921de9c42b934787b1e28f64
Christine <christine.herlihy@gmail.com>
2016-01-07 15:09:24 -0500

    Update Euler_Problem4.py

--- Euler_Problem4.py
+++ Euler_Problem4.py
@@ -2,18 +2,13 @@
 # The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-# A palindromic number reads the same both ways.
-# The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
-# Find the largest palindrome made from the product of two 3-digit numbers.
-
 def palindrome(num):
     """
    :rtype : boolean
     """
     nums = str(num)
 
-    # Recursive stopping rule: a one-digit number is a palindrome; even numbers will result in an empty string
-
+    # Recursive stopping rule: a one-digit number is a palindrome; two-digits are if both digits match
     if len(nums) == 1 | 0:
         print("true")
         return True
@@ -25,16 +20,12 @@ def palindrome(num):
 
     else:
         if nums[0] == nums[len(nums) - 1]:
-
             nums = nums[1:len(nums) - 1]
-
-            # Recursive function call
             palindrome(nums)
-
+            
         else:
             print("false")
             return False
-
 
 # Test cases:
 mylist = [90109, 0, 401, 33, 696, 100, 2442, 35653, 190]

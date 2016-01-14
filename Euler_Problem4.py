af56b8654e7fb5b03baefce21d625c34b6f896a0
Christine <christine.herlihy@gmail.com>
2016-01-14 15:52:03 -0500

    Solved problem 4

--- Euler_Problem4.py
+++ Euler_Problem4.py
@@ -6,6 +6,8 @@
 # Define functions #
 # -----------------_#
 
+#palindrome returns TRUE if the parameter is a palindrome and FALSE if it is not
+# @param: num = an integer to inspect
 def palindrome(num):
     """
    :rtype : boolean
@@ -14,13 +16,14 @@ def palindrome(num):
 
     # Recursive stopping rule: a one-digit number is a palindrome; even numbers will result in an empty string
 
-    if len(nums) == 1 | 0:
-        print("True")
+    if len(nums) == 1| 0:
+        #print("True")
         return True
+
 
     elif len(nums) == 2:
         if nums[0] == nums[1]:
-            print("True")
+            #print("True")
             return True
 
         else:
@@ -31,8 +34,10 @@ def palindrome(num):
         if nums[0] == nums[len(nums) - 1]:
             nums = nums[1:len(nums) - 1]
 
-            # Recursive function call
-            palindrome(nums)
+
+            # Note: in Python, for this recursive call to work, you can't just call the func; you have to include "return"
+            # Otherwise, if it goes here, it will return "None"
+            return palindrome(nums)
 
         else:
             # print("false")
@@ -44,38 +49,34 @@ def palindrome(num):
 
 # Need to account for possibility that it might be the number times itself.
 
-
+# findLargestPal finds the largest palindrome that is the product of 2 three-digit numbers within a given range
+# @param: start: an integer; represents the max you want to start with; each pass through the outer loop will decrement
+# @param: num = an integer to inspect
 def findLargestPal(start):
+    """
+       :rtype : int
+    """
     templist = []
+    mybool = False
     for i in range(start, 899, -1):
         for j in range(start, 899, -1):
             temp = i * j
             # print(i , j)
-            print(temp)
+            # print(temp)
             if palindrome(temp):
                 templist.append(temp)
-                print(temp)
-                return templist
-
+                print(i, j, temp)
+    return templist[0]
 
 
 # Test cases for palindrome function:
-mylist = [24, 90109, 0, 401, 33, 696, 100, 2442, 35653, 190]
+mylist = [24, 90109, 0, 401, 33, 696, 100, 2442, 35653, 190, 990009]
 
 for x in mylist:
-    print(x)
+ print(palindrome(x))
 
-
-
-
-list2 = [1]
-list2= findLargestPal(999)
-
-for x in list2:
-    print(x)
-
-
-
-
+#Run the function, starting at 999 (largest 3 digit number)
+print(findLargestPal(999))
 
 #ANSWER IS: 906609
+#Product of: 993 * 913
